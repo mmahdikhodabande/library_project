b09dc3b16701bb3ec92b14bfa195582ca6f1b646
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2023-10-27 10:07:15 -0800

    all changes for library project

--- library_project/urls.py
+++ library_project/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home_module.urls')),
     path('products/', include('product_module.urls')),
+    path('contact-us/', include('contact_module.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
